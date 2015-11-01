7ae99e78fd56f2fa0b8f197006fb8633b5c4b63e
Andres Araya <araya.andres@gmail.com>
2015-11-01 17:50:30 -0600

    allow user pick number of throws

--- js/scripts.js
+++ js/scripts.js
@@ -37,11 +37,10 @@ Floorboard.prototype.contain = function(p) {
     return this.x < p.x && p.x < (this.x + this.width);
 }
 
-function Needle(center, alpha, length, color) {
+function Needle(center, alpha, length) {
     this.center = center || new Point(0, 0);
     this.alpha = alpha || 0;
     this.length = length || 1;
-    this.color = 'black';
 }
 
 Needle.prototype.getP1 = function () {
@@ -65,32 +64,43 @@ Needle.prototype.inside = function(floorboard) {
     return floorboard.contain(this.getP1()) && floorboard.contain(this.getP2());
 }
 
-const WIDTH = 300, HEIGHT = 300;
-var canvas = $('#canvas')[0];
-var ctx = canvas.getContext('2d');
-var floorboard = new Floorboard(WIDTH / 3, 0, WIDTH / 3, HEIGHT);
-var needle;
-var needleLength = floorboard.width / 3;
-var numberOfThrows = 1000, hits = 0, error, pi, i;
-var msg = '';
+function throwNeedles(numberOfThrows) {
+    const WIDTH = 300, HEIGHT = 300;
+    var numberOfThrows = numberOfThrows || 100;
+    var canvas = $('#canvas')[0];
+    var ctx = canvas.getContext('2d');
+    var floorboard = new Floorboard(WIDTH / 3, 0, WIDTH / 3, HEIGHT);
+    var needle;
+    var needleLength = floorboard.width / 3;
+    var hits = 0, pi;
 
-canvas.width = WIDTH;
-canvas.height = HEIGHT;
-floorboard.draw(ctx);
-for (i = 0; i < numberOfThrows; ++i) {
-    needle = new Needle(new Point(getRandomInt(floorboard.x, floorboard.x + floorboard.width),
-                                  getRandomInt(0, floorboard.height)),
-                        getRandomInt(-90, 90),
-                        needleLength);
-    needle.draw(ctx);
-    if (!needle.inside(floorboard)) {
-        hits++;
+    canvas.width = WIDTH;
+    canvas.height = HEIGHT;
+    floorboard.draw(ctx);
+    for (i = 0; i < numberOfThrows; ++i) {
+        needle = new Needle(new Point(getRandomInt(floorboard.x, floorboard.x + floorboard.width),
+                                      getRandomInt(0, floorboard.height)),
+        getRandomInt(-90, 90),
+        needleLength);
+        needle.draw(ctx);
+        if (!needle.inside(floorboard)) {
+            hits++;
+        }
     }
+    pi = 2 * needleLength * numberOfThrows / (floorboard.width * hits);
+    $('#throws_value').text(numberOfThrows);
+    $('#hits').text(hits);
+    $('#pi').text(pi);
+    $('#error').text((100 * (pi - Math.PI) / Math.PI) + '%');
 }
-pi = 2 * needleLength * numberOfThrows / (floorboard.width * hits);
-error = 100 * (pi - Math.PI) / Math.PI;
-msg += '<p>number of throws = ' + numberOfThrows + '</p>';
-msg += '<p>number of hits = ' + hits + '</p>';
-msg += '<p>pi = ' + pi + '</p>';
-msg += '<p>error = ' + error + '%</p>';
-$(msg).insertAfter($('#canvas'));
+
+var run = function ()
+{
+    var n = $('#throws').val();
+    var hits = throwNeedles(parseInt(n));
+}
+
+$(document).ready(function () {
+    $('#throws').click(run);
+    run();
+});
