b80b7c3b37c0023eb44d7262c0787d7fce3801b0
Andres Araya <araya.andres@gmail.com>
2015-11-01 22:02:23 -0600

    mvc

--- js/scripts.js
+++ js/scripts.js
@@ -64,41 +64,68 @@ Needle.prototype.inside = function(floorboard) {
     return floorboard.contain(this.getP1()) && floorboard.contain(this.getP2());
 }
 
-function draw(width, height, floorboard, needles, hits, pi, error) {
-    var canvas = $('#canvas')[0];
-    var ctx = canvas.getContext('2d');
-    canvas.width = width;
-    canvas.height = height;
-    floorboard.draw(ctx);
-    needles.forEach(function (needle) { needle.draw(ctx); });
-    $('#throws_value').text(needles.length);
-    $('#hits').text(hits);
-    $('#pi').text(pi);
-    $('#error').text(error + '%');
+function Buffon(floorboard) {
+    this.floorboard = floorboard;
+    this.numberOfThrows = 1000;
+    this.needles = [];
+    this.hits = 0;
+    this.pi = 0.0;
+    this.error = 0.0;
 }
 
-var run = function () {
-    var width = height = 600;
-    var floorboard = new Floorboard(width / 3, 0, width / 3, height);
-    var numberOfThrows = parseInt($('#throws').val());
-    var needle, needles = [], needleLength = floorboard.width / 3;
-    var hits = 0, pi, error, i;
-
-    for (i = 0; i < numberOfThrows; ++i) {
+Buffon.prototype.calc = function () {
+    var needleLength = this.floorboard.width / 3, needle, i;
+    var floorboard = this.floorboard;
+    this.needles = [];
+    this.hits = 0;
+    for (i = 0; i < this.numberOfThrows; ++i) {
         needle = new Needle(new Point(getRandomInt(floorboard.x, floorboard.x + floorboard.width),
                                       getRandomInt(0, floorboard.height)),
                             getRandomInt(-90, 90),
                             needleLength);
-        if (!needle.inside(floorboard)) hits++;
-        needles.push(needle);
+        if (!needle.inside(floorboard)) this.hits++;
+        this.needles.push(needle);
     }
-    pi = 2 * needleLength * numberOfThrows / (floorboard.width * hits);
-    error = 100 * (pi - Math.PI) / Math.PI;
-    draw(width, height, floorboard, needles, hits, pi, error);
+    this.pi = 2 * needleLength * this.numberOfThrows / (floorboard.width * this.hits);
+    this.error = 100 * (this.pi - Math.PI) / Math.PI;
+}
+
+function UI(canvas, numberOfThrows, hits, pi, error) {
+    const WIDTH = 600,
+          HEIGHT = 600;
+    this.canvas = canvas;
+    this.numberOfThrows = numberOfThrows;
+    this.hits = hits;
+    this.pi = pi;
+    this.error = error;
+    this.canvas.width = WIDTH;
+    this.canvas.height = HEIGHT;
+    this.floorboard = new Floorboard(WIDTH / 3, 0, WIDTH / 3, HEIGHT);
+    this.buffon = new Buffon(this.floorboard);
+}
+
+UI.prototype.draw = function () {
+    var ctx = canvas.getContext('2d');
+    var buffon = this.buffon;
+    ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.numberOfThrows.text(buffon.numberOfThrows);
+    this.hits.text(buffon.hits);
+    this.pi.text(buffon.pi);
+    this.error.text(buffon.error);
+    this.floorboard.draw(ctx);
+    buffon.needles.forEach(function (needle) { needle.draw(ctx); });
+}
+
+UI.prototype.update = function(numberOfThrows) {
+    this.buffon.numberOfThrows = numberOfThrows;
+    this.buffon.calc();
+    this.draw();
 }
 
 $(document).ready(function () {
-    $('#throws').click(run);
-    $('#throw_button').click(run);
-    run();
+    var ui = new UI($('#canvas')[0], $('#throws_value'), $('#hits'), $('#pi'), $('#error'));
+    var callback = function () { ui.update(parseInt($('#throws').val())); };
+    $('#throws').click(callback);
+    $('#throw_button').click(callback);
+    callback();
 });
